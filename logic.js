7617868a61e35e01c39573f9b6c9867221661578
preston <prstnwhite@gmail.com>
2022-11-04 21:12:52 -0700

    having sequential addition working, but other stuff not so good. Need to refactor

--- logic.js
+++ logic.js
@@ -1,25 +1,89 @@
 
+resultBox = document.querySelector('.result-box')
+
+let currentResult = 0;
+let currentNum = 0;
+let postOperandClick = false;
+let enteredNums = 0;
+let lastOperand = ''
 
 function operate(operand, num1, num2){
 
 
-    if(operand === 'add'){
+    if(operand === '+'){
 
         return(num1 + num2)
 
-    } else if (operand === 'subtract'){
+    } else if (operand === '-'){
 
         return(num1 - num2)
 
-    } else if (operand === 'multiply'){
+    } else if (operand === '*'){
 
         return(num1 * num2)
         
-    } else if (operand === 'divide'){
+    } else if (operand === '/'){
 
         return(num1 / num2)
         
     }
 }
 
-console.log("pickles")
+function getNum(){
+    
+    currentNum = Number.parseInt(resultBox.innerHTML)
+}
+
+document.querySelectorAll('.button')
+        .forEach( btn => {
+            btn.addEventListener( 'click', event => {
+                if(postOperandClick){
+                    resultBox.innerHTML = ''
+                    postOperandClick = false
+                }
+                    
+            resultBox.innerHTML += event.target.innerHTML
+
+            })
+        })
+
+document.querySelector('.clear-button')
+        .addEventListener( 'click', event => {
+            resultBox.innerHTML = ""
+            liveCalc = false;
+            enteredNums = 0;
+            currentNum = 0;
+            currentResult = 0;
+        })
+
+document.querySelectorAll('.op-button')
+        .forEach( btn => {
+            btn.addEventListener( 'click', event => {
+
+                if(enteredNums === 0){
+                    getNum()
+                    currentResult = currentNum
+                }
+
+                if(enteredNums > 0){
+                    getNum()
+                    currentResult = operate(event.target.innerHTML, currentResult, currentNum)
+                    resultBox.innerHTML = currentResult
+                }
+                enteredNums += 1
+                postOperandClick = true;
+                console.log('operator clicked')
+
+                lastOperand = event.target.innerHTML
+            })
+        })
+
+document.querySelector('.calc-button').addEventListener('click', event => {
+        getNum()
+        currentResult = operate(lastOperand, currentResult, currentNum)
+        currentNum = 0;
+        resultBox.innerHTML = currentResult
+        enteredNums += 1
+        postOperandClick = true;
+        lastOperand = ''
+    })
