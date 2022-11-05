7661a4d608c3087c0c441bb81f96819a4dd649cd
preston <prstnwhite@gmail.com>
2022-11-04 19:23:23 -0700

    initial set up, barebonse html, css and js - linked to index.html

---
+++ logic.js
@@ -0,0 +1,25 @@
+
+
+function operate(operand, num1, num2){
+
+
+    if(operand === 'add'){
+
+        return(num1 + num2)
+
+    } else if (operand === 'subtract'){
+
+        return(num1 - num2)
+
+    } else if (operand === 'multiply'){
+
+        return(num1 * num2)
+        
+    } else if (operand === 'divide'){
+
+        return(num1 / num2)
+        
+    }
+}
+
+console.log("pickles")
